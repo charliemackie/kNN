b4024dba4d0115ebe95619f1a46c00f8623b44ad
charliemackie <cmacki43@uwo.ca>
2020-01-07 21:56:04 -0500

    charlie

---
+++ Iris.py
@@ -0,0 +1,111 @@
+import numpy as np
+from sklearn import datasets
+
+# Load the Iris Data from SkLearn
+
+iris = datasets.load_iris()
+iris_data = iris.data
+iris_labels = iris.target
+
+# Split and organize the data into a random array
+
+np.random.seed(42)
+indices = np.random.permutation(len(iris_data))
+n_training_samples = 12
+learnset_data = iris_data[indices[:-n_training_samples]]
+learnset_labels = iris_labels[indices[:-n_training_samples]]
+testset_data = iris_data[indices[-n_training_samples:]]
+testset_labels = iris_labels[indices[-n_training_samples:]]
+
+# Visualize our data set
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Given 4 metrics for each data point (Info about each Iris)
+# Want to see the data in 3D space so we will add the 3rd and 4th values
+
+colours = ("r", "b")
+X = []
+for iclass in range(3):
+    X.append([[], [], []])
+    for i in range(len(learnset_data)):
+        if learnset_labels[i] == iclass:
+            X[iclass][0].append(learnset_data[i][0])
+            X[iclass][1].append(learnset_data[i][1])
+            X[iclass][2].append(sum(learnset_data[i][2:]))
+
+colours = ("r", "g", "y")
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for iclass in range(3):
+    ax.scatter(X[iclass][0], X[iclass][1], X[iclass][2], c=colours[iclass])
+plt.show()  # Show the 3D plot of the data
+
+
+# Distance method will determine the Euclidean distance between two points
+
+def distance(instance1, instance2):
+    # just in case, if the instances are lists or tuples:
+    instance1 = np.array(instance1)
+    instance2 = np.array(instance2)
+
+    return np.linalg.norm(instance1 - instance2)
+
+
+# get_neighbors will return the 'k' nearest neighbors to a given point
+
+def get_neighbors(training_set,
+                  labels,
+                  test_instance,
+                  k,
+                  distance=distance):
+    """
+    get_neighors calculates a list of the k nearest neighbors
+    of an instance 'test_instance'.
+    The list neighbors contains 3-tuples with
+    (index, dist, label)
+    where
+    index    is the index from the training_set,
+    dist     is the distance between the test_instance and the
+             instance training_set[index]
+    distance is a reference to a function used to calculate the
+             distances
+    """
+    distances = []
+
+    for index in range(len(training_set)):
+        dist = distance(test_instance, training_set[index])
+        distances.append((training_set[index], dist, labels[index]))
+    distances.sort(key=lambda x: x[1])  # Distance from every instance
+    neighbors = distances[:k]  # the neighbors are specified by k
+    return neighbors
+
+
+from collections import Counter
+
+
+# Vote method will aggregate the neighbors and find the most common
+
+def vote(neighbors):
+    class_counter = Counter()
+    for neighbor in neighbors:
+        class_counter[neighbor[2]] += 1
+    return class_counter.most_common(1)[0][0]
+
+
+# Test
+
+
+for i in range(n_training_samples):
+    neighbors = get_neighbors(learnset_data,
+                              learnset_labels,
+                              testset_data[i],
+                              3,
+                              distance=distance)
+    print("index: ", i,
+          ", result of vote: ", vote(neighbors),
+          ", label: ", testset_labels[i],
+          ", data: ", testset_data[i])
